097a813e3b692c4d1a8512c96d0d709a7a6ead3c
JAgusOckier <juanockier@gmail.com>
2025-05-02 19:12:07 -0300

    fix/navbar: desplazamiento a secciones solucionado

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,11 +20,11 @@ const Navbar = () => {
     <nav className="fixed top-0 left-0 w-full bg-foreground/10 backdrop-blur-md z-50">
       <div className="w-full overflow-x-auto scrollbar-hide">
         <ul className="flex min-w-max justify-center gap-6 text-sm sm:text-md tracking-wide px-4 py-2 whitespace-nowrap">
-          <li><button onClick={() => scrollToSection('about')} className="block">Sobre mí</button></li>
-          <li><button onClick={() => scrollToSection('projects')} className="block">Proyectos</button></li>
-          <li><button onClick={() => scrollToSection('education')} className="block">Educación</button></li>
-          <li><button onClick={() => scrollToSection('tech')} className="block">Tecnologías</button></li>
-          <li><button onClick={() => scrollToSection('contact')} className="block">Contacto</button></li>
+          <li><button onClick={() => scrollToSection('about')} className="block cursor-pointer">Sobre mí</button></li>
+          <li><button onClick={() => scrollToSection('projects')} className="block cursor-pointer">Proyectos</button></li>
+          <li><button onClick={() => scrollToSection('education')} className="block cursor-pointer">Educación</button></li>
+          <li><button onClick={() => scrollToSection('tech')} className="block cursor-pointer">Tecnologías</button></li>
+          <li><button onClick={() => scrollToSection('contact')} className="block cursor-pointer">Contacto</button></li>
         </ul>
       </div>
     </nav>
